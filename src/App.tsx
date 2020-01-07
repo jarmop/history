bacce4b3e1d875981d3f0f00bbf15490ef449cef
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2020-01-07 20:43:45 +0200

    Initial create react app skeleton with TypeScript and CSS Modules.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.css';
+
+const App: React.FC = () => {
+    return (
+        <div className={styles.app}>
+            History app
+        </div>
+    );
+};
+
+export default App;
