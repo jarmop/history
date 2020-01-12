cc8533269e679810e819f9bbbd3f27aca2ecddff
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2020-01-12 23:23:43 +0200

    Updates

---
+++ precache-manifest.484c3ba270d7bedaa50832bacaec6af0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61ba1f54f97095e59eb322d9e6eb594",
+    "url": "/history/index.html"
+  },
+  {
+    "revision": "534e2e097996816bb927",
+    "url": "/history/static/css/main.29d80b71.chunk.css"
+  },
+  {
+    "revision": "3b8388c8ca11d406698d",
+    "url": "/history/static/js/2.8adc01fb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/history/static/js/2.8adc01fb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "534e2e097996816bb927",
+    "url": "/history/static/js/main.f372e40b.chunk.js"
+  },
+  {
+    "revision": "35f2f3112e6bbb130d84",
+    "url": "/history/static/js/runtime-main.2d6f554a.js"
+  }
+]);
