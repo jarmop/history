7ad0acccdf643fced09ef193870870fed17b18ad
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2022-05-08 11:25:01 +0300

    Zoom to middle east on the map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -4,5 +4,9 @@ import React from 'react'
 import map from '../maps/blank-map-world-simple.svg'
 
 export const Map: React.FC = () => { 
-  return <img className={ styles.map} src={map}/>
+  return (
+    <div className={styles.container}>
+      <img className={styles.map} src={map} />
+    </div>
+  )
 }
